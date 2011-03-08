e0b47eb3e0adb852b2b377b32298cd4cc85ffd63
Yichun Zhang (章亦春) <agentzh@gmail.com>
2011-03-08 08:36:47 +0000

    Import of AGENT/Test-Nginx-0.13 from CPAN.
    gitpan-cpan-distribution: Test-Nginx
    gitpan-cpan-version:      0.13
    gitpan-cpan-path:         AGENT/Test-Nginx-0.13.tar.gz
    gitpan-cpan-author:       AGENT
    gitpan-cpan-maturity:     released

--- lib/Test/Nginx/LWP.pm
+++ lib/Test/Nginx/LWP.pm
@@ -4,7 +4,7 @@ use lib 'lib';
 use lib 'inc';
 use Test::Base -Base;
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 our $NoLongString;
 
--- lib/Test/Nginx/Util.pm
+++ lib/Test/Nginx/Util.pm
@@ -3,7 +3,7 @@ package Test::Nginx::Util;
 use strict;
 use warnings;
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 use base 'Exporter';
 
